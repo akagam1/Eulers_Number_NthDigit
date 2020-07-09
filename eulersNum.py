f3a3e5d6dfa2f75d6e890e6415d7f25c32ece6fd
Arjun Puthli <45913008+akagam1@users.noreply.github.com>
2020-07-09 15:53:22 +0530

    Add files via upload

---
+++ eulersNum.py
@@ -0,0 +1,21 @@
+def factorial(n):
+	if n == 0:
+		return(1)
+	else:
+		return(n*factorial(n-1))
+
+def euler(m):
+	e = 1
+	for x in range(1,21):
+		e = e + 1/factorial(x)
+	e = round(e, m)
+	return e
+
+print(euler(int(input("Number of decimals: "))))
+
+
+
+
+
+
+
